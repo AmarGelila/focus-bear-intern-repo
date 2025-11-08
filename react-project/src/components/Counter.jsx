19d29e7cd0d210b1948481ee81fc08a79f56f11e
AmarGelila <merogelila@gmail.com>
2025-11-08 10:49:31 +0200

    Add issue-41.js

--- react-project/src/components/Counter.jsx
+++ react-project/src/components/Counter.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 export default function Counter() {
-  const [counter, setCounter] = useState(0);
+  let counter = 0;
   return (
     <div className="text-center p-5">
       <h1 className="text-blue-500 font-bold mb-2">Current count: {counter}</h1>
       <button
-        onClick={() => setCounter((prev) => prev + 1)}
+        onClick={() => {
+          counter++;
+        }}
         className="px-3 py-1 border-2 border-blue-500 text-3xl font-black rounded-2xl text-blue-500 hover:bg-blue-500 hover:text-white bg-white hover:cursor-pointer"
       >
         +
